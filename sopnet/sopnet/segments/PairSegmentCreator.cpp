3a15d1e7324e2e441fc5ce3178a99e14bbce3352
Anna Kreshuk <anna.kreshuk@iwr.uni-heidelberg.de>
2014-07-02 22:18:12 +0200

    one more step

--- sopnet/sopnet/segments/PairSegmentCreator.cpp
+++ sopnet/sopnet/segments/PairSegmentCreator.cpp
@@ -1,6 +1,9 @@
 #include "PairSegmentCreator.h"
 #include "vigra/impex.hxx"
 #include <vigra/multi_array.hxx>
+#include <vigra/accumulator.hxx>
+#include <vigra/labelimage.hxx>
+#include <util/rect.hpp>
 
 /*
 PairSegmentCreator::PairSegmentCreator(
@@ -76,18 +79,65 @@ PairSegmentCreator::readSynapseMap() {
 
 void
 PairSegmentCreator::groupBySynapse() {
+
+	using namespace vigra::acc;
+	int interval_index = 0; //awful, isnt' it?
 	foreach(boost::shared_ptr<Image> im, _synapse_map) {
 
-		vigra::MultiArray<2, float> bin(vigra::Shape2(im->width(), im->height()));
+		vigra::MultiArray<2, unsigned char> bin(vigra::Shape2(im->width(), im->height()));
+		vigra::MultiArray<2, unsigned int> cc(vigra::Shape2(im->width(), im->height()));
 
-		vigra::Threshold<float, float> vthreshold(0.5, 0.5, 0, 1);
-		bin = vthreshold(*im);
+		vigra::transformMultiArray(*im, bin, vigra::Threshold<Image::value_type, int>(0.5, 1, 0, 1));
+		unsigned int total_synapses = vigra::labelImageWithBackground(bin, cc, false, 0);
+		std::cout<<total_synapses<<" found by thresholding in image "<<interval_index<<std::endl;
+		//find the segments of this image
+		//FIXME: only continuations for now?
 
-		//vigra::transformImage(im->upperLeft(), im->lowerRight(), im->accessor(),
-		 //                     bin.upperLeft(), bin.accessor(),
-		  //                    vigra::Threshold<im::difference_type_1, vigra::Uint8>(0.5, 0.5, 0, 1));
+		//for each synapse: 1) take the mask, 2) dilate by 1 or 2, 3) create ConnectedComponent
+		// 4) for each segment, check intersections on the first slice
+
+		//How to store the groups? do we need one-on-one neighborhood relations as well?
+
+
+		std::vector<boost::shared_ptr<ContinuationSegment> > cont_segments;
+		cont_segments = _neuron_segments.getContinuations(interval_index);
+
+		//find the synapse bounding boxes
+		AccumulatorChainArray<vigra::CoupledArrays<2, Image::value_type, unsigned int>,
+		                             Select<DataArg<1>, LabelArg<2>,
+		                             Coord<Minimum>, Coord<Maximum> > > acc;
+		acc.ignoreLabel(0);
+
+		extractFeatures(*im, cc, acc);
+
+		//unsigned int min = get<Coord<Minimum> >(acc, 1)[0];
+		//std::cout<<"min for the first synapse:"<<min<<std::endl;
+
+		std::vector<std::vector<boost::shared_ptr<Segment> > > synapse_groups(total_synapses);
+		for (unsigned int isyn =0; isyn<total_synapses; isyn++) {
+			util::rect<unsigned int> syn_bbox;
+			syn_bbox.minX = get<Coord<Minimum> >(acc, isyn)[0];
+			syn_bbox.minY = get<Coord<Minimum> >(acc, isyn)[1];
+			syn_bbox.maxX = get<Coord<Maximum> >(acc, isyn)[0];
+			syn_bbox.maxY = get<Coord<Maximum> >(acc, isyn)[1];
+			foreach(boost::shared_ptr<Segment> segment, cont_segments) {
+				boost::shared_ptr<ConnectedComponent> first_slice_cc = segment->getSourceSlices()[0]->getComponent();
+				//check, if bounding boxes overlap
+				util::rect<int> slice_bbox = first_slice_cc->getBoundingBox();
+				if (syn_bbox.intersects(slice_bbox)){
+					synapse_groups[isyn].push_back(segment);
+				}
+			}
+			std::cout<<"synapse "<<isyn<<", bounding box: "<<syn_bbox.minX<<", "<<syn_bbox.minY<<", "<< syn_bbox.maxX<<", "<<syn_bbox.maxY<<std::endl;
+			std::cout<<"synapse "<<isyn<<", "<<synapse_groups[isyn].size()<<" segments overlap in bounding boxes"<<std::endl;
+
+
+		}
+
+
 
 
 		std::cout<<"PAIR SEGMENT CREATOR: processed another image "<<im->width()<<" "<<im->height()<<std::endl;
+		interval_index++;
 	}
 }
